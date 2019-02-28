d2c934625e70a7a5b1f2d208a70f99bb050a0275
Viral Tandel <viral.b.tandel@gmail>
2019-02-27 19:22:48 -0800

    first git commit for selenium code

---
+++ src/test/java/ecommerceTest/DemoTest2.java
@@ -0,0 +1,43 @@
+package ecommerceTest;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	@Test
+	public void sum() {
+		int a = 20;
+		int b = 10;
+		System.out.println("SUM");
+		assertEquals(30, a + b);
+		
+	}
+
+	@Test
+	public void sub() {
+		int a = 20;
+		int b = 10;
+		System.out.println("SUB");
+		assertEquals(10, a - b);
+	}
+
+	@Test
+	public void mult() {
+		int a = 20;
+		int b = 10;
+		System.out.println("MULT");
+		assertEquals(200, a * b);
+	}
+
+	@Test
+	public void div() {
+		int a = 20;
+		int b = 10;
+		System.out.println("DIV");
+		assertEquals(2, a / b);
+	System.out.println("=========================================");
+	}
+
+}
